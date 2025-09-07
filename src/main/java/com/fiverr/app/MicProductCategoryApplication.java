4d4cf7f316f7883951fc6dd73387bdc06e2ff57d
Nexaplafrom <nexaplaform@gmail.com>
2025-09-07 17:22:54 +0200

    Configurando BD

--- src/main/java/com/fiverr/app/MicProductCategoryApplication.java
+++ src/main/java/com/fiverr/app/MicProductCategoryApplication.java
@@ -2,8 +2,10 @@ package com.fiverr.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin(origins = "http://mic-productcategory-production.up.railway.app")
 public class MicProductCategoryApplication {
 
 	public static void main(String[] args) {

499750f0d9d7910e31ca70512d145823a142d5d0
Nexaplafrom <nexaplaform@gmail.com>
2025-09-07 17:43:29 +0200

    Configurando BD

--- src/main/java/com/fiverr/app/MicProductCategoryApplication.java
+++ src/main/java/com/fiverr/app/MicProductCategoryApplication.java
@@ -4,10 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@OpenAPIDefinition(servers = {
-//        @Server(url = "/", description = "Default Server"),
-//        @Server(url = "https://mic-productcategory-production.up.railway.app/", description = "Production Server")
-//})
 public class MicProductCategoryApplication {
 
 	public static void main(String[] args) {

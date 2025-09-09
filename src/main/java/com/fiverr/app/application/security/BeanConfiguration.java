7a550df125a5e78982bb338a526d2151921d0215
Nexaplafrom <nexaplaform@gmail.com>
2025-09-09 07:25:31 +0200

    Configurando Cors

--- src/main/java/com/fiverr/app/application/security/BeanConfiguration.java
+++ src/main/java/com/fiverr/app/application/security/BeanConfiguration.java
@@ -1,45 +1,24 @@
 package com.fiverr.app.application.security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
-import java.util.List;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class BeanConfiguration {
+public class BeanConfiguration implements WebMvcConfigurer {
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedOrigins(List.of(
-                "http://localhost:4200",
-                "http://mic-productcategory-production.up.railway.app",
-                "https://mic-productcategory-production.up.railway.app"
-        ));
-        corsConfiguration.setAllowedMethods(Arrays.asList(
-                HttpMethod.GET.name(),
-                HttpMethod.POST.name(),
-                HttpMethod.PUT.name(),
-                HttpMethod.DELETE.name(),
-                HttpMethod.PATCH.name(),
-                HttpMethod.OPTIONS.name()
-        ));
-        corsConfiguration.setAllowedHeaders(Arrays.asList(
-                HttpHeaders.CONTENT_TYPE,
-                HttpHeaders.AUTHORIZATION,
-                HttpHeaders.ACCEPT,
-                HttpHeaders.ORIGIN
-        ));
-        corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setMaxAge(3600L);
-        source.registerCorsConfiguration("/**", corsConfiguration);
-        return source;
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods(HttpMethod.GET.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.DELETE.name(),
+                        HttpMethod.PATCH.name(),
+                        HttpMethod.OPTIONS.name())
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }

d0a6c62cc6f9f5317addd773018caea532197de8
Nexaplafrom <nexaplaform@gmail.com>
2025-09-09 15:16:59 +0200

    Agregando nombre y descripcion a las operaciones

--- src/main/java/com/fiverr/app/MicProductCategoryApplication.java
+++ src/main/java/com/fiverr/app/MicProductCategoryApplication.java
@@ -8,8 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @OpenAPIDefinition(servers = {
         @Server(url = "/", description = "Default Server"),
-        @Server(url = "https://mic-productcategory-production.up.railway.app/", description = "Production Server"),
-        @Server(url = "http://localhost:8080", description = "Local Server")
+        @Server(url = "https://mic-productcategory-production.up.railway.app/", description = "Production Server")
 })
 public class MicProductCategoryApplication {
 
--- src/main/java/com/fiverr/app/api/service/BaseApi.java
+++ src/main/java/com/fiverr/app/api/service/BaseApi.java
@@ -12,6 +12,9 @@ import java.util.List;
 public interface BaseApi<I, O, K> {
 
     @PostMapping
+    @Operation(
+            summary = "Create record",
+            description = "Creation of record")
     default ResponseEntity<O> create(@Valid @RequestBody I dto) {
         return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
     }
@@ -19,13 +22,15 @@ public interface BaseApi<I, O, K> {
     @GetMapping({"/{id}"})
     @Operation(
             summary = "Get record by id",
-            description = "Search a record by id"
-    )
+            description = "Search a record by id")
     default ResponseEntity<O> findById(@PathVariable K id) {
         return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
     }
 
     @PutMapping("/id")
+    @Operation(
+            summary = "Update record",
+            description = "Update a record by id")
     default ResponseEntity<O> update(@Valid @RequestBody I dto, Long id) {
         return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
     }
@@ -33,18 +38,19 @@ public interface BaseApi<I, O, K> {
     @DeleteMapping({"/{id}"})
     @Operation(
             summary = "Delete record",
-            description = "Delete a record by id"
-    )
+            description = "Delete a record by id")
     default ResponseEntity<Void> deleteById(@PathVariable K id) {
-        return new ResponseEntity(HttpStatus.NOT_IMPLEMENTED);
+        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
     }
 
     @GetMapping
+    @Operation(
+            summary = "Get paginated records",
+            description = "Get a number of paginated records")
     default ResponseEntity<List<O>> findAll(
             @RequestParam(defaultValue = "0") Integer page,
             @RequestParam(defaultValue = "9") Integer size,
-            @RequestParam(defaultValue = "ASC") SortEnumDTO sort
-    ) {
+            @RequestParam(defaultValue = "ASC") SortEnumDTO sort) {
         return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
     }
 }
